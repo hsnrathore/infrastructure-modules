e9bc7dd07ec7d2c911cd9325bc1c7ac089a0ace1
Hassan Rathore <hsnrathore@gmail.com>
2022-10-11 01:27:45 +0300

    Fixed Secuurity Group

--- alb/main.tf
+++ alb/main.tf
@@ -120,13 +120,13 @@ resource "aws_security_group" "alb" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
-  #   egress {
-  #    protocol         = "-1"
-  #    from_port        = 0
-  #    to_port          = 0
-  #    cidr_blocks      = ["0.0.0.0/0"]
-  #    ipv6_cidr_blocks = ["::/0"]
-  #   }
+    egress {
+     protocol         = "-1"
+     from_port        = 0
+     to_port          = 0
+     cidr_blocks      = ["0.0.0.0/0"]
+     ipv6_cidr_blocks = ["::/0"]
+    }
   tags = {
     Terraform   = "true"
     Environment = "${var.environment}"
